3834db8c1f2e87b69fa7ad194b3910cf20f812dd
Krista Loven <krista.loven.87@gmail.com>
2019-09-30 10:40:51 -0700

    generate resources and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :post_tags
+  resources :comments
+  resources :blogs
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
