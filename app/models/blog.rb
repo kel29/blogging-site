42a6d4590c677631ae6fdf534b44ee8c2901b2e1
Krista Loven <krista.loven.87@gmail.com>
2019-09-30 10:52:51 -0700

    establish model associations and initial validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,8 @@
 class Blog < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  validates :post_id, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :content, presence: true
 end
