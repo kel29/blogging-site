161e1ea15a2dd55e694ec2328674b6c3df713487
Krista Loven <krista.loven.87@gmail.com>
2019-09-30 17:16:23 -0700

    add show page for user with route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/tags/:id', to: 'tags#show', as: 'tags'
+  get '/users/:id', to: 'users#show', as: 'users'
   resources :comments
   resources :blogs
-  resources :users
 end
