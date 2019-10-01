f5bd5af59b46b35f689fdbf7e5e1af97962f60e9
Krista Loven <krista.loven.87@gmail.com>
2019-09-30 17:04:55 -0700

    added show page for tags and adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :tags
+  get '/tags/:id', to: 'tags#show', as: 'tags'
   resources :comments
   resources :blogs
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
