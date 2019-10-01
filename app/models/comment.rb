82d6dae9594d2443d00933e6842db2b711bd4171
Krista Loven <krista.loven.87@gmail.com>
2019-10-01 15:22:29 -0700

    remove validation for commentor uniqueness

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :blog
   validates :blog_id, presence: true
   validates :commentator, presence: true
-  validates :commentator, uniqueness: true
   validates :content, presence: true
 end
