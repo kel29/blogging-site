1075314254f8c04fe329a3ba95098a1fc64cae12
Krista Loven <krista.loven.87@gmail.com>
2019-10-01 14:53:46 -0700

    add commenting functionality to blog posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    byebug
+    blog = Blog.find(params[:blog_id])
+    @new_comment = Comment.new(params.permit(:commentator, :content, :blog_id))
+    if @new_comment.save
+      redirect_to blog
+    else
+      render blog
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/tags/:id', to: 'tags#show', as: 'tags'
   get '/users/:id', to: 'users#show', as: 'users'
   get '/like/:id', to: 'blogs#like', as: 'like'
-  resources :comments
-  resources :blogs
+  resources :blogs do
+    resources :comments, only: [:create, :destroy]
+  end
 end
