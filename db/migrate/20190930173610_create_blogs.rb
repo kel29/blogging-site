2a1be38b29bab55ecb4cefa336b1f8fc1705f562
Krista Loven <krista.loven.87@gmail.com>
2019-10-02 12:11:55 -0700

    update blogs create table

--- db/migrate/20190930173610_create_blogs.rb
+++ db/migrate/20190930173610_create_blogs.rb
@@ -3,6 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[5.1]
     create_table :blogs do |t|
       t.string :title
       t.string :content
+      t.integer :user_id
       t.integer :likes
 
       t.timestamps
