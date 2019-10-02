99ab4c7131bcb48aa8552bab749791a55615f89d
Krista Loven <krista.loven.87@gmail.com>
2019-10-01 17:45:06 -0700

    restrict delete to admin authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :admin_logged_in?, :redirect
 
   def redirect(location)
     redirect_to location unless admin_logged_in?
