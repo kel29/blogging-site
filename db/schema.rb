c6104df0a7ec6dadb1d77c832c9624f2588f2c00
Krista Loven <krista.loven.87@gmail.com>
2019-10-03 21:19:09 -0700

    add spacing to start of pages, reseed with subtitles, adjust link hover based on background color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191002011637) do
+ActiveRecord::Schema.define(version: 20191004005929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20191002011637) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitle"
   end
 
   create_table "blogs_tags", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
  
-5.times do
+User.create(name: "Krista Loven", email: "krista.loven.87@gmail.com", password: "1234", password_confirmation: "1234")
+
+15.times do
   Blog.create(
     title: Faker::Hipster.sentence(word_count: 5),
-    content: Faker::Hipster.paragraph(sentence_count: 15),
+    subtitle: Faker::Hipster.paragraph(sentence_count: 3),
+    content: Faker::Hipster.paragraphs(number: 10),
     likes: 0,
     user_id: 1
   )
 end
  
-8.times do
+30.times do
   Comment.create(
     commentator: Faker::FunnyName.name,
     content: Faker::Hipster.sentence(word_count: 7),
-    blog_id: rand(1..5)
+    blog_id: rand(1..15)
   )
 end
  
-10.times do
+30.times do
   Tag.create(tag_name: Faker::Hipster.word)
 end
  
-20.times do
+40.times do
   BlogTag.create(
-    blog_id: rand(1..5),
-    tag_id: rand(1..10)
+    blog_id: rand(1..15),
+    tag_id: rand(1..30)
   )
 end
